f034d7702236f6fc2977029952ff3a5769b4e3a9
4MN35T <tanmaykarmore01@gmail.com>
2023-03-06 16:03:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
+  const [expenses, setExpenses] = useState([]);
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [expenseInput, setExpenseInput] = useState('');
+
+  const handleExpenseInput = (event) => {
+    setExpenseInput(event.target.value);
+  };
+
+  const handleAddExpense = () => {
+    const expense = expenseInput.split(' - ');
+    const expenseName = expense[0];
+    const expensePrice = parseFloat(expense[1]);
+
+    const newExpense = {
+      name: expenseName,
+      price: expensePrice
+    };
+
+    setExpenses([...expenses, newExpense]);
+    setTotalExpense(totalExpense + expensePrice);
+    setExpenseInput('');
+  };
 
   return (
-    <div id="main">
-      <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
-      <div id="expense-list">
+    <div id='main'>
+      <input
+        id='expense-input'
+        value={expenseInput}
+        onChange={handleExpenseInput}
+      />
+      <button id='expense-button' onClick={handleAddExpense}>
+        Add Expense
+      </button>
+      <div id='expense-list'>
+        {expenses.map((expense, index) => (
+          <div key={index}>
+            {expense.name} - {expense.price}
+          </div>
+        ))}
       </div>
-      <div id="total-expense">
-        Total Expense: 0
-      </div>
+      <div id='total-expense'>Total Expense: {totalExpense}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
